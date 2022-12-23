5a0c68ea8841bf3620b1c43610240538dc2cfcb5
Dan Mattern <dan@danmattern.com>
2022-12-23 14:04:37 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {HttpClient} from '@actions/http-client'
 
+interface GetIpResponse {
+  ip: string
+}
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const apiKey: string = core.getInput('api_key')
+    const endpointUrl: string = core.getInput('endpoint_url')
+    const http = new HttpClient('get-ip-action', undefined, {
+      allowRetries: true,
+      maxRetries: 2
+    })
+    http.requestOptions = {
+      headers: {
+        'x-api-key': apiKey
+      }
+    }
+    const ip = await http.getJson<GetIpResponse>(endpointUrl)
+    core.setOutput('ip', ip.result?.ip)
+    core.info(`ip: ${ip.result?.ip}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
